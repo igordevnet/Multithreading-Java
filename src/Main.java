af45fcd988dac10ac05f87dc20a9f24d07bf10f0
IGOR ALMEIDA <igor.almeida55@etec.sp.gov.br>
2026-06-18 03:34:43 -0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main
+{
+    public static void main(String[] args) {
+
+        Runnable r1 = () -> {
+            for (int i=0; i<10;++i) {
+                System.out.println("Thread 3: " + i);
+            }
+        };
+
+        Runnable r2 = () -> {
+            for (int i=0; i<10;++i) {
+                System.out.println("Thread 4: " + i);
+            }
+        };
+
+        var t1 = new Thread(new Runner1());
+        var t2 = new Thread(new Runner2());
+        var t3 = new Thread(r1);
+        var t4 = new Thread(r2);
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+
+    }
+}
---
+++ src/Runner1.java
@@ -0,0 +1,10 @@
+public class Runner1 implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i=0; i<10;++i) {
+            System.out.println("Thread 1: " + i);
+        }
+    }
+}
+
