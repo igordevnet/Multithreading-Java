1509fceb51a742336082279fce41a580b1490f65
igordevnet <igorsouzaalmeida2404@gmail.com>
2026-07-09 14:20:45 -0300

    Understanding How to Stop a Thread Pool and Ensure the Application Shuts Down Safely

---
+++ ExecutorShutdown/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+
+        for(int i=0;i<100;i++) {
+            executor.execute(new Task(i));
+        }
+
+        executor.shutdown();
+
+        try {
+            if(!executor.awaitTermination(1000, TimeUnit.MILLISECONDS)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException ex) {
+            executor.shutdown();
+        }
+
+    }
+}
